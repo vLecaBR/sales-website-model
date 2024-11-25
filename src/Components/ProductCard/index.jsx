8bade068875ca90f0fc4bbc115894ea6aa07fdb6
vlecabr <vitartasleca@gmail.com>
2024-11-25 10:54:08 -0300

    comentarios no código

--- src/Components/ProductCard/index.jsx
+++ src/Components/ProductCard/index.jsx
@@ -10,25 +10,24 @@ import {
   CartButton
 } from './ProductCard.styles';
 
-const ProductCard = ({ product, setCartItems }) => {
-  const addToCart = () => {
-    setCartItems((prevItems) => {
-      const itemIndex = prevItems.findIndex((item) => item.id === product.id);
-      if (itemIndex > -1) {
-        const updatedItems = [...prevItems];
-        updatedItems[itemIndex].quantity += 1;
-        return updatedItems;
-      } else {
-        return [...prevItems, { ...product, quantity: 1 }];
-      }
-    });
-  };
+  const ProductCard = ({ product, setCartItems }) => {
+    const addToCart = () => {
+      setCartItems((prevItems) => {
+        const itemIndex = prevItems.findIndex((item) => item.id === product.id);
+        if (itemIndex > -1) {
+          const updatedItems = [...prevItems];
+          updatedItems[itemIndex].quantity += 1;
+          return updatedItems;
+        } else {
+          return [...prevItems, { ...product, quantity: 1 }];
+        }
+      });
+    };
 
-  return (
+  return ( //! Adiciona o botão de adicionar ao carrinho e o botão de ver detalhes
     <ProductContainer>
       <ProductImage src={product.image} alt={product.name} />
       <ProductName>{product.name}</ProductName>
-      {/* Verifique se priceCash existe antes de usar toFixed */}
       <ProductPrice>
         R$ {product.price ? product.price.toFixed(2) : '0.00'}
       </ProductPrice>
