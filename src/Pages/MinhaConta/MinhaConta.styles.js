a7a98ffd592d6beebc6449f8116d10cc437414ec
vlecabr <vitartasleca@gmail.com>
2024-11-27 15:48:23 -0300

    comentario adicionado

--- src/Pages/MinhaConta/MinhaConta.styles.js
+++ src/Pages/MinhaConta/MinhaConta.styles.js
@@ -1,6 +1,7 @@
 // src/Pages/MinhaConta.styles.js
 import styled from 'styled-components';
 
+// Container da página
 export const Container = styled.div`
   max-width: 800px;
   margin: 20px auto;
@@ -11,11 +12,13 @@ export const Container = styled.div`
   margin-top: 5vh;
 `;
 
+// Título da página
 export const Title = styled.h1`
   text-align: center;
   color: #000; /* Cor do texto */
 `;
 
+// Seção da página
 export const Section = styled.div`
   margin: 20px 0;
   padding: 10px;
@@ -24,11 +27,13 @@ export const Section = styled.div`
   box-shadow: 0 1px 5px rgba(0, 0, 0, 0.1);
 `;
 
+// Título da seção
 export const SectionTitle = styled.h2`
   margin-bottom: 10px;
   color: #8445FF; /* Cor do título */
 `;
 
+//Botões
 export const Button = styled.button`
   padding: 10px 15px;
   margin-top: 10px;
