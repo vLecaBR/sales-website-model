5fd302678757691d7793b32836523b0ce817f951
vlecabr <vitartasleca@gmail.com>
2024-11-28 11:32:06 -0300

    comentários estilos appjs

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -2,18 +2,21 @@
 
 import styled from 'styled-components';
 
+//! Container geral da aplicação
 export const AppContainer = styled.div`
   display: flex;
   flex-direction: column;
   min-height: 100vh;
 `;
 
+//! Container principal
 export const MainContainer = styled.main`
   padding: 1.5%;
   flex-grow: 1;
   margin-bottom: 25px;
 `;
 
+//! Adiciona o estilo do título
 export const Title = styled.h1`
   text-align: center;
   margin: 2% 0;
@@ -24,6 +27,7 @@ export const Title = styled.h1`
   }
 `;
 
+//! Adiciona o estilo do grid de produtos
 export const ProductsGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
