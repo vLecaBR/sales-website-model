ace93e8e83bffa735a48221b8f56c83a67d2cae5
vlecabr <vitartasleca@gmail.com>
2025-03-27 14:15:07 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 //! App.jsx
 
-
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
